2c40e6238cb9147e638c613f57e35cd375565d80
Fabian Born <Fabian.Born@sage.com>
2020-11-26 16:31:57 +0100

    First UI entwurf

--- TransportFinderView/MainWindow.xaml.cs
+++ TransportFinderView/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SwissTransport;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TransportFinderView
 {
@@ -20,9 +10,53 @@ namespace TransportFinderView
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Transport _transport;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _transport = new Transport();
+        }
+
+        private void MeineLocation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+        private void StationSuche_KeyDown(object sender, KeyEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            if (e.Key == Key.Return )
+            {
+
+                //Abfahrtsstation.Items.Clear();
+                //     Ankunftsstation.Items.Clear();
+
+                string StationName = comboBox.Text;
+
+                Stations stations = _transport.GetStations(StationName);
+
+
+                
+
+                foreach (Station station in stations.StationList)
+                {
+                    _ = comboBox.Name == "Abfahrtsstation"
+                        ? Abfahrtsstation.Items.Add(station.Name)
+                        : Ankunftsstation.Items.Add(station.Name);
+                }
+            }
+
         }
     }
 }
